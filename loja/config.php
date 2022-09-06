4279ed8d56af98cc482745cf9a18663871cf8879
Vinícius Giovanne Gonçalves <lim4ovgg@gmail.com>
2022-09-06 16:44:33 -0300

    parte 2
    alterando a extensão das paginas e a senha do mysql
    
    Co-Authored-By: Vinícius Giovanne Gonçalves <109826016+LIM4Ovgg@users.noreply.github.com>

--- loja/config.php
+++ loja/config.php
@@ -2,7 +2,7 @@
 
 	$dbHost = 'Localhost';
 	$dbUsername = 'root';
-	$dbPassword = 'Vini2002';
+	$dbPassword = '';
 	$dbName = 'loja';
 
 	$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
