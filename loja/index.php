e56375d415108ed0c269c93bf21064fedaee70fb
Vinícius Giovanne Gonçalves <lim4ovgg@gmail.com>
2022-09-06 18:10:24 -0300

    Update index.php
    Co-Authored-By: Vinícius Giovanne Gonçalves <109826016+LIM4Ovgg@users.noreply.github.com>

--- loja/index.php
+++ loja/index.php
@@ -35,7 +35,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/estilos.css">
 
-    <title>Tech Dev Online :: Página Principal</title>
+    <title></title>
 </head>
 
 <body>
